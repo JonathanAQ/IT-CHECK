62b3cbfd36313a615b26d53d5fe27c1c9efc1011
JonathanAQ <jonathan.araujo.queiroz@gmail.com>
2017-10-22 16:28:41 -0300

    Update MENU V1[003]
    Nessa atualização foram realizadas algumas modificações visuais e correção de um bug no controle de decisão com as opções do menu.

--- MENU V1[002].bat
+++ MENU V1[002].bat
@@ -1,37 +1,42 @@
 
 
 
-: menu
+:menu
 
 cls
 
-ECHO   =====================================================================
-ECHO  #	                          * MENU *                    				# 																		
-ECHO  #         1. ANALISE TECNICA DE COMPUTADOR (SEM UPDATES)				#
-ECHO  #				- Verificacao de rede;									#
-ECHO  #				- Status da maquina;									#
-ECHO  #				- Gravacao de relatorio .txt							#																			
-ECHO  # 	    2. ANALISE TECNICA DE COMPUTADOR (COM UPDATES)				#	
-ECHO  #				- Verificacao de rede;									#
-ECHO  #				- Status da maquina;									#
-ECHO  #				- Atualizacao dos softwares (Adobe, FlashPlayer, Java)	#
-ECHO  #				- Gravacao de relatorio .txt							#																		
-ECHO   ===================================================================== 
+ECHO =======================================================================================================
+ECHO  	                              * MENU *
+ECHO          		1. ANALISE TECNICA DE COMPUTADOR (SEM UPDATES)
+ECHO  					- Verificacao de rede;
+ECHO  					- Status da maquina;
+ECHO  					- Gravacao de relatorio .txt
+ECHO   	    		2. ANALISE TECNICA DE COMPUTADOR (COM UPDATES)
+ECHO  					- Verificacao de rede;
+ECHO  					- Status da maquina;
+ECHO  					- Atualizacao dos softwares (Adobe, FlashPlayer, Java);
+ECHO  					- Gravacao de relatorio .txt
+ECHO   	    		3. SAIR
+ECHO ======================================================================================================== 
 
-SET /P OPCAO = ESCOLHA A OPCAO DESEJADA
-
-if %OPCAO% equ 1 goto opcao1
-if %OPCAO% equ 2 goto opcao2
-if %OPCAO% GEQ 3 goto exceptionMenu
+set /p opcao= Escolha uma opcao: 
+echo ------------------------------
+if %opcao% equ 1 goto opcao1
+if %opcao% equ 2 goto opcao2
+if %opcao% equ 3 goto opcao3
+if %opcao% GEQ 6 goto opcao6
 
 {
 	BLOCO COM OPCAO 1 E 2
 }
 
-:exceptionMenu
+:opcao3
+cls
+exit
 
-ECHO ==============================================
-ECHO * Opcao Invalida! Escolha outra opcao do menu *
-ECHO ==============================================
+:opcao6
+echo ==============================================
+echo * Opcao Invalida! Escolha outra opcao do menu *
+echo ==============================================
 pause
 goto menu
